483cd6b82f796856080b9f9b639aaf22b24ecceb
ShengyinWu <icylord@gmail.com>
2017-05-25 13:22:37 +0800

    Update fastfile_helper.h

--- include/spdlog/details/fastfile_helper.h
+++ include/spdlog/details/fastfile_helper.h
@@ -46,10 +46,6 @@ namespace details
 {
 
 #ifdef _WIN32
-	static bool Truncate(HANDLE fd, long long file_size)
-	{
-		return SetFilePointer(fd, file_size, 0, FILE_BEGIN) && SetEndOfFile(fd);
-	}
 	static void Truncate(filename_t filename, long long file_size)
 	{
 		int fd = _open(filename.c_str(), O_RDWR);
@@ -70,13 +66,13 @@ public:
     const int open_interval = 10;
 
     explicit fastfile_helper() :
-		m_offset_(0)
+		offset_(0)
     {
 #ifndef _WIN32
-		m_fd_ = -1;
+		fd_ = -1;
 #else
-		m_fd_ = INVALID_HANDLE_VALUE;
-		m_map_ = INVALID_HANDLE_VALUE;
+		fd_ = INVALID_HANDLE_VALUE;
+		mmap_ = INVALID_HANDLE_VALUE;
 #endif
 	}
 
@@ -91,7 +87,7 @@ public:
 
     void open(const filename_t& fname, long long max_size, bool truncate = false)
     {
-		m_maxsize_ = max_size;
+		max_size_ = max_size;
 		_filename = fname;
 		close();
 		if (truncate)
@@ -101,35 +97,57 @@ public:
 		if (os::file_exists(fname))
 		{
 			FILE *p = fopen(fname.c_str(), "r");
-			m_offset_ = os::filesize(p);
+			offset_ = os::filesize(p);
 			fclose(p);
 		}
 
 #ifdef _WIN32
-		m_fd_ = CreateFile(fname.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+		fd_ = CreateFile(fname.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
 			OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-		if (INVALID_HANDLE_VALUE == m_fd_)
+		if (INVALID_HANDLE_VALUE == fd_)
 		{
 			throw spdlog_ex("Failed opening file " + os::filename_to_str(_filename) + " for writing", errno);
 		}
 		// If CreateFileMapping is called to create a file mapping object for hFile,
 		// UnmapViewOfFile must be called first to unmap all views and call CloseHandle to close the file mapping object
 		// before you can call SetEndOfFile.
-		m_map_ = CreateFileMapping(m_fd_, NULL, PAGE_READWRITE, 0, m_maxsize_, NULL);
-		if (NULL == m_map_)
+		mmap_ = CreateFileMapping(fd_, NULL, PAGE_READWRITE, 0, max_size_, NULL);
+		if (NULL == mmap_)
 		{
-			CloseHandle(m_fd_);
-			m_fd_ = INVALID_HANDLE_VALUE;
+			CloseHandle(fd_);
+			fd_ = INVALID_HANDLE_VALUE;
 			throw spdlog_ex("Failed opening file " + os::filename_to_str(_filename) + " for writing", errno);
 		}
-		m_buf_ = (char *)MapViewOfFile(m_map_, FILE_MAP_ALL_ACCESS, 0, m_offset_, m_maxsize_);
-		if (NULL == m_buf_)
+		buf_ = (char *)MapViewOfFile(mmap_, FILE_MAP_ALL_ACCESS, 0, offset_, max_size_);
+		if (NULL == buf_)
 		{
-			CloseHandle(m_map_); // close the file mapping object
-			m_map_ = INVALID_HANDLE_VALUE;
-			CloseHandle(m_fd_);
-			m_fd_ = INVALID_HANDLE_VALUE;
+			CloseHandle(mmap_); // close the file mapping object
+			mmap_ = INVALID_HANDLE_VALUE;
+			CloseHandle(fd_);
+			fd_ = INVALID_HANDLE_VALUE;
 			Truncate(fname, 0);
+			throw spdlog_ex("Failed opening file " + os::filename_to_str(_filename) + " for writing", errno);
+		}
+#else
+		fd_ = ::open(m_file_
+			, O_CREAT | O_APPEND | O_RDWR
+			, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+		if (fd_ < 0)
+		{
+			throw spdlog_ex("Failed opening file " + os::filename_to_str(_filename) + " for writing", errno);
+		}
+		if (::ftruncate(fd_, max_size_) != 0)
+		{
+			::close(fd_);
+			fd_ = -1;
+			throw spdlog_ex("Failed opening file " + os::filename_to_str(_filename) + " for writing", errno);
+		}
+		buf_ = (char *)::mmap(0, max_size_, PROT_WRITE, MAP_SHARED, fd_, offset_);
+		if (buf_ == MAP_FAILED)
+		{
+			::ftruncate(fd_, 0);
+			::close(fd_);
+			fd_ = -1;
 			throw spdlog_ex("Failed opening file " + os::filename_to_str(_filename) + " for writing", errno);
 		}
 #endif // _WIN32
@@ -151,14 +169,16 @@ public:
     void close()
     {
 #ifdef _WIN32
-		FlushViewOfFile(m_buf_, m_offset_);
-		UnmapViewOfFile(m_buf_);
-		CloseHandle(m_map_); // close the file mapping object
-		CloseHandle(m_fd_);
-		m_fd_ = INVALID_HANDLE_VALUE;
-		Truncate(_filename, m_offset_);
+		FlushViewOfFile(buf_, offset_);
+		UnmapViewOfFile(buf_);
+		CloseHandle(mmap_); // close the file mapping object
+		CloseHandle(fd_);
+		fd_ = INVALID_HANDLE_VALUE;
+		Truncate(_filename, offset_);
 		int a = 1;
 #else
+		::ftruncate(fd_, offset_);
+		::close(fd_);
 #endif
     }
 
@@ -167,13 +187,13 @@ public:
 
         size_t msg_size = msg.formatted.size();
         auto data = msg.formatted.data();
-		memcpy(m_buf_ + m_offset_, data, msg_size);
-		m_offset_ += msg_size;
+		memcpy(buf_ + offset_, data, msg_size);
+		offset_ += msg_size;
     }
 
     size_t size()
     {
-		return m_offset_;
+		return offset_;
     }
 
     const filename_t& filename() const
@@ -188,14 +208,14 @@ public:
     }
 
 private:
-	long long m_maxsize_;
-	int m_offset_; // free offset
-	char *m_buf_; // mmap dest
+	size_t max_size_;
+	int offset_; // free offset
+	char *buf_; // mmap dest
 #ifndef _WIN32
-	int m_fd_; // log file descriptor
+	int fd_; // log file descriptor
 #else
-	HANDLE m_fd_;
-	HANDLE m_map_;
+	HANDLE fd_;
+	HANDLE mmap_;
 #endif
     filename_t _filename;
 };
